d174163343074c1946f0f1aadb27b20b85bc6697
Steve Turley <steve@turleylabs.com>
2020-07-25 15:37:39 -0500

    Adding enough combinations to cover all branches

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
 import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
@@ -10,9 +9,9 @@ public class GildedRoseTest {
 
     @Test
     public void approveGildedRose() {
-        String[] nameChoices = {"my item"};
-        Integer[] sellInChoices = {0};
-        Integer[] qualityChoices = {0};
+        String[] nameChoices = {"my item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+        Integer[] sellInChoices = {-1, 0, 11};
+        Integer[] qualityChoices = {0, 49, 50};
         CombinationApprovals.verifyAllCombinations(this::updateQuality, nameChoices, sellInChoices, qualityChoices);
     }
 
