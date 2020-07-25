a5635500697a4b4bd92b136b1f8611d0e5904aa9
Steve Turley <steve@turleylabs.com>
2020-07-25 15:45:57 -0500

    Reverting mutation and updating tests to prevent mutation failure.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,8 +10,8 @@ public class GildedRoseTest {
     @Test
     public void approveGildedRose() {
         String[] nameChoices = {"my item", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-        Integer[] sellInChoices = {-1, 0, 11};
-        Integer[] qualityChoices = {0, 49, 50};
+        Integer[] sellInChoices = {-1, 0, 5, 6, 10, 11};
+        Integer[] qualityChoices = {0, 1, 49, 50};
         CombinationApprovals.verifyAllCombinations(this::updateQuality, nameChoices, sellInChoices, qualityChoices);
     }
 
